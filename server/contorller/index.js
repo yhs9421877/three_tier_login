007556e8082e1e3b4b7f1ebc94a129b6f4b37c7b
HyesungYoon <yhs9421877@gmail.com>
2022-02-05 18:48:46 +0900

    옵션: timestamps false 추가

--- server/contorller/index.js
+++ server/contorller/index.js
@@ -5,8 +5,12 @@ module.exports = {
   login: {
     post: async (req,res) => {
       const {id,pwd} = req.body;
-      // console.log(req.body)
-      const result = User.findAll()
+      console.log(req.body);
+      try{
+        const result = User.findAll();
+       } catch(err) {
+        console.error("error: ", err);
+       }
       // const temp = await Model.findOne({ where: { userId: id}})
       // res.end();
       res.redirect('http://localhost:3000/')
